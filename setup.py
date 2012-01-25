2b0758a847b4c770c3e3d5feda31d4a6bf5537e2
unknown <Den@.(none)>
2012-01-25 13:22:52 +0400

    initial

---
+++ setup.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+VERSION = (0, 1, 0)
+__version__ = VERSION
+__versionstr__ = '.'.join(map(str, VERSION))
+
+setup(
+    name='sq-subscribe',
+    version=__versionstr__,
+    description="sq_subscribe описание",
+    author='SevenQuark',
+    author_email='info@sevenquark.com',
+    url='http://www.sevenquark.com',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+)
---
+++ sq_subscribe_tests/settings.py
@@ -0,0 +1,79 @@
+import os
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+ADMINS = ()
+
+MANAGERS = ADMINS
+
+PROJECT_ROOT = os.path.dirname(os.path.realpath(__file__))
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'sqlite.db',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    }
+}
+
+TIME_ZONE = 'Europe/Moscow'
+
+LANGUAGE_CODE = 'ru-RU'
+
+SITE_ID = 1
+
+USE_I18N = True
+
+USE_L10N = True
+
+MEDIA_ROOT = ''
+
+MEDIA_URL = ''
+
+STATIC_ROOT = ''
+
+STATIC_URL = '/static/'
+
+ADMIN_MEDIA_PREFIX = '/static/admin/'
+
+STATICFILES_DIRS = ()
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
+
+SECRET_KEY = 's+m51jp(bx380!u#0f8owd-(n6_))%-332r1xg6z14)@_w+7c3'
+
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+)
+
+ROOT_URLCONF = 'urls'
+
+TEMPLATE_DIRS = (
+    os.path.join(PROJECT_ROOT, 'templates')
+)
+
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.admin',    
+)
