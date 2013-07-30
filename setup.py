57aafd888909cce2bd94e880f6f17ea8977417ea
n.zykov <n.zykov@sv-lab.ru>
2013-07-30 11:07:43 +0600

    edit version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
 
-VERSION = (0, 5, 1)
+VERSION = (0, 5, 2)
 __version__ = VERSION
 __versionstr__ = '.'.join(map(str, VERSION))
 
