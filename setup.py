cbae052b181ac26bb2aa8ebc9f45ee71edb8e3f1
Ivan Varnavsky <astron.x-ray@mail.ru>
2013-08-13 16:53:11 +0600

    v0.5.40

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
 
-VERSION = (0, 5, 39)
+VERSION = (0, 5, 40)
 __version__ = VERSION
 __versionstr__ = '.'.join(map(str, VERSION))
 
