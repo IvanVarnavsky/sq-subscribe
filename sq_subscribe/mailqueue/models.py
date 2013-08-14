22cde911bf029a0b02a7c56ab403c63a4888ed0c
Ivan Varnavsky <astron.x-ray@mail.ru>
2013-08-14 17:02:24 +0600

    v0.5.76

--- sq_subscribe/mailqueue/models.py
+++ sq_subscribe/mailqueue/models.py
@@ -10,9 +10,7 @@ from django.db import models
 from django.template.loader import render_to_string, get_template_from_string
 from django.utils.html import strip_tags
 from django.conf import settings
-from django.core.files import File
 from apps.main.utils import ATTACHMENT_PATH
-from django.core.files.storage import FileSystemStorage, Storage
 import os
 
 
@@ -41,7 +39,6 @@ class MailQueue(models.Model):
 
 
     def send_email(self,connecion):
-        print"-----<3>-----"
         vars = json.loads(self.message)
         template_directory =  getattr(settings, "EMAIL_TEMPLATE_DIR", 'email')
         if self.template.endswith('.html') or self.template.endswith('.txt'):
@@ -72,56 +69,18 @@ class MailQueue(models.Model):
                     msg = EmailMessage(self.subject,text_content,from_email=self.send_from,to=[send_to_list[0]],connection=connecion, bcc=send_to_list[1:], headers={'Cc': ','.join(send_to_list[1:])})
                 else:
                     msg = EmailMessage(self.subject,text_content,from_email=self.send_from,to=[send_to_list[0]],connection=connecion)
-            print vars
             if ('attachment' in vars['data']) and (vars['data']['attachment']!={}):
-                print vars['data']['attachment']['att_file_name']
-                print vars['data']['attachment']['att_file_type']
-                print vars['data']['attachment']['att_file_path']
-                print '1------------'
                 path = vars['data']['attachment']['att_file_path']
-                print '2------------'
-                #f = open(path, 'r')
                 with open(path, 'r') as f:
-                    print '3------------'
-                    #with open('attachments/meeting.ics', 'r') as f:
-                    #with open(vars['data']['attachment']['att_file_path']) as f:
-                    print "open"
-                    # for line in f.readlines():
-                    #     print line
-
-                    # attfile = File(f)
-                    # attfile.read()
-
-                    #attfile.write(f)
-                    attfile = f.read()
-                    print '4------------'
-                    #attfile = File(f.read())
-                    #attfile = ContentFile(f.read())
-                    #attfile = ContentFile(f)
-                    msg.attach(vars['data']['attachment']['att_file_name'], attfile, vars['data']['attachment']['att_file_type'])
-                    print '5------------'
-                    print "close..."
-                    #attfile.closed
-                    print '6------------'
+                    msg.attach(vars['data']['attachment']['att_file_name'], f.read(), vars['data']['attachment']['att_file_type'])
                 f.closed
-                print "closed"
-                #f.delete()
-                #File.delete(f)
-                #File.delete(path)
-                #FileSystemStorage.delete(f)
-                #FileSystemStorage.delete(path)
-                #Storage.delete(f)
-                #Storage.delete(path)
-                #delete(path)
                 os.remove(path)
-                print "delete"
         except Exception:
             raise Exception('Email message %s can not created.'%self.id)
         self.delete()
         return msg
 
 def create_mailqueue(subject, template, send_to, content_type, message=None, send_from=None, attachment=None):
-    print"-----<2>-----"
     if not message: message = {}
     if not attachment: attachment = {}
     if send_from is None:
@@ -135,34 +94,13 @@ def create_mailqueue(subject, template, send_to, content_type, message=None, sen
 
 
 def send_email(subject,template,send_to,content_type,message=None,send_from=None,attachment=None):
-    print"-----<1>-----"
-    #attachment = {'att_file_name': att_file_name, 'att_file': att_file, 'att_file_type': att_file_type}
-    print attachment
     if attachment is not None:
-        #att_data = json.dumps(attachment)
-        print attachment['att_file_name']
-        print attachment['att_file']
-        print attachment['att_file_type']
         attach_t = {}
-        #att_directory = u'/media/attachments/'
-        #att_directory = ATTACHMENT_PATH
         att_file_path = ATTACHMENT_PATH + u'/' + attachment['att_file_name']
-        print att_file_path
         with open(att_file_path, 'w') as f:
-        #with open(str(att_file_dir).encode("utf-8"), 'w') as f:
-        #with codecs.open(os.path.join(att_directory, att_file_name), "w", "utf-8") as f:
-        #with open('meeting.ics', 'w') as f:
-            print "open"
-            #f = File(att_file)
-            # myfile = File(f)
-            # myfile.write('Hello World')
-            # myfile.closed
             f.write(attachment['att_file'])
-            print "close..."
             f.closed
-            print "closed"
             attach_t.update({'att_file_name': attachment['att_file_name'], 'att_file_path': att_file_path, 'att_file_type': attachment['att_file_type']})
-            print attach_t
 
     from sq_subscribe.mailqueue.tasks import send_concrete_mailqueue
     mail = create_mailqueue(subject,template,send_to,content_type,message,send_from,attach_t)
